9c89d16966c3cc9ec4612f81aecd5bf8dfacda98
doTruongGiang-IT <dotruonggiang290@gmail.com>
2021-12-20 17:11:50 +0700

    [Add]: Init project

---
+++ foreign_language_center/src/main/java/com/sgu/foreign_language_center/ForeignLanguageCenterApplication.java
@@ -0,0 +1,13 @@
+package com.sgu.foreign_language_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignLanguageCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeignLanguageCenterApplication.class, args);
+	}
+
+}
---
+++ foreign_language_center/src/test/java/com/sgu/foreign_language_center/ForeignLanguageCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgu.foreign_language_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignLanguageCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
