1f829c4d70d85aeac4e49bf7ad2b5ca61f822873
doTruongGiang-IT <dotruonggiang290@gmail.com>
2021-12-27 17:25:52 +0700

    [Add]: Create DTO Examination

--- winform/language_center_winform/src/DTO/ExaminationDTO.java
+++ winform/language_center_winform/src/DTO/ExaminationDTO.java
@@ -1,5 +1,39 @@
 package DTO;
 
 public class ExaminationDTO {
-
+	private int id;
+	private String name;
+	private String examDate;
+	
+	public ExaminationDTO(){};
+	
+	public ExaminationDTO(int id, String name, String examDate) {
+		this.id = id;
+		this.name = name;
+		this.examDate = examDate;
+	};
+	
+	public int getId() {
+		return id;
+	};
+	
+	public void setId(int id) {
+		this.id = id;
+	};
+	
+	public int getName() {
+		return name;
+	};
+	
+	public void setName(String name) {
+		this.name = name;
+	};
+	
+	public int getExamDate() {
+		return examDate;
+	};
+	
+	public void setExamDate(String examDate) {
+		this.examDate = examDate;
+	};
 }
