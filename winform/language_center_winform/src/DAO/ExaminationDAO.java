1425d9812e07e8ec947df2b76838f663e9c1afd4
doTruongGiang-IT <dotruonggiang290@gmail.com>
2021-12-28 18:12:22 +0700

    [Add]: Create ExaminationDAO

--- winform/language_center_winform/src/DAO/ExaminationDAO.java
+++ winform/language_center_winform/src/DAO/ExaminationDAO.java
@@ -1,5 +1,89 @@
 package DAO;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import org.json.JSONObject;
+import DTO.ExaminationDTO;
+
 public class ExaminationDAO {
 
+	private List<ExaminationDTO> examinations = null;
+	private String API_URL = "";
+	
+	public ExaminationDAO() {};
+	
+	public List<ExaminationDTO> getExaminations() throws Exception {
+		examinations = new ArrayList<ExaminationDTO>();
+		HttpClient client = HttpClient.newHttpClient();
+	    HttpRequest request = HttpRequest.newBuilder()
+	            .uri(new URI(API_URL))
+	            .headers("Content-Type", "application/json;charset=UTF-8")
+	            .GET()
+	            .build();
+	    HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JSONObject responseObj = new JSONObject(response.body().toString());
+        System.out.println(responseObj);
+        return examinations;
+    };
+    
+    public ExaminationDTO getExamination(int id) throws Exception {
+    	HttpClient client = HttpClient.newHttpClient();
+	    HttpRequest request = HttpRequest.newBuilder()
+	            .uri(new URI(API_URL))
+	            .headers("Content-Type", "application/json;charset=UTF-8")
+	            .GET()
+	            .build();
+	    HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JSONObject responseObj = new JSONObject(response.body().toString());
+        System.out.println(responseObj);
+        return null;
+    };
+    
+    public void insert(ExaminationDTO examination) throws Exception {
+        HashMap<String, Object> newExamination = new HashMap<String, Object>();
+        newExamination.put("name", examination.getName());
+        newExamination.put("date", examination.getDate());
+        HttpClient client = HttpClient.newHttpClient();
+	    HttpRequest request = HttpRequest.newBuilder()
+	            .uri(new URI(API_URL))
+	            .headers("Content-Type", "application/json;charset=UTF-8")
+	            .POST(HttpRequest.BodyPublishers.ofString(newExamination.toString()))
+	            .build();
+	    HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JSONObject responseObj = new JSONObject(response.body().toString());
+        System.out.println(responseObj);
+    };
+    
+    public void update(ExaminationDTO examination) throws Exception {
+        HashMap<String, Object> updateExamination = new HashMap<String, Object>();
+        updateExamination.put("name", examination.getName());
+        updateExamination.put("date", examination.getDate());
+        HttpClient client = HttpClient.newHttpClient();
+	    HttpRequest request = HttpRequest.newBuilder()
+	            .uri(new URI(API_URL))
+	            .headers("Content-Type", "application/json;charset=UTF-8")
+	            .PUT(HttpRequest.BodyPublishers.ofString(updateExamination.toString()))
+	            .build();
+	    HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JSONObject responseObj = new JSONObject(response.body().toString());
+        System.out.println(responseObj);
+    };
+    
+    public void delete(int id) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+	    HttpRequest request = HttpRequest.newBuilder()
+	            .uri(new URI(API_URL))
+	            .headers("Content-Type", "application/json;charset=UTF-8")
+	            .DELETE()
+	            .build();
+	    HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JSONObject responseObj = new JSONObject(response.body().toString());
+        System.out.println(responseObj);
+    };
+   
 }
