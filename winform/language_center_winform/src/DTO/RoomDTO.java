f36d5505ae731c9570fb395072272a0527252f09
doTruongGiang-IT <dotruonggiang290@gmail.com>
2021-12-28 16:54:55 +0700

    [Add]: Create RoomDTO

---
+++ winform/language_center_winform/src/DTO/RoomDTO.java
@@ -0,0 +1,69 @@
+package DTO;
+
+public class RoomDTO {
+	private int id;
+	private int examination_id;
+	private String name;
+	private String level;
+	private String time;
+	
+	public RoomDTO() {}
+
+	public RoomDTO(int id, int examination_id, String name, String level, String time) {
+		super();
+		this.id = id;
+		this.examination_id = examination_id;
+		this.name = name;
+		this.level = level;
+		this.time = time;
+	};
+
+	public RoomDTO(int examination_id, String name, String level, String time) {
+		super();
+		this.examination_id = examination_id;
+		this.name = name;
+		this.level = level;
+		this.time = time;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getExamination_id() {
+		return examination_id;
+	}
+
+	public void setExamination_id(int examination_id) {
+		this.examination_id = examination_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	};
+	
+}
