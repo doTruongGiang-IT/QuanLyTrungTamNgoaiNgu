67c8b648c8f7c653e592ccf31c87deb288277ee9
Kiều Đặng Kim Bảo <kieudangkimbao@gmail.com>
2021-12-29 16:42:43 +0700

    [Add] : tao CRUD Supervior API

--- backend/Program.cs
+++ backend/Program.cs
@@ -9,6 +9,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<QLTTNNContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddControllers();
 builder.Services.AddScoped<ICandidateRepository, CandidateRepository>();
+builder.Services.AddScoped<ISupervisorRepository, SupervisorRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
