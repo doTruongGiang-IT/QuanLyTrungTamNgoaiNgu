1724b38dbfd6ace50c4e6bb6ac481248a865f789
longhop02 <71638082+longhop02@users.noreply.github.com>
2022-01-03 20:15:30 +0700

    [Add] : Create gui manage supervisor

---
+++ winform/language_center_winform/src/GUI/SupervisorPanel.java
@@ -0,0 +1,152 @@
+package GUI;
+
+import javax.swing.JPanel;
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.BoxLayout;
+import java.awt.BorderLayout;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.border.TitledBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+
+public class SupervisorPanel extends JPanel {
+	private JTable tableSupervisor;
+	private JTextField textSupervisorName;
+	private JTextField textSearch;
+
+	/**
+	 * Create the panel.
+	 */
+	public SupervisorPanel() {
+		setMaximumSize(new Dimension(1039, 763));
+		setLayout(new CardLayout(0, 0));
+		
+		JPanel panelContent = new JPanel();
+		add(panelContent, "name_452838340029000");
+		panelContent.setLayout(new GridLayout(2, 0, 0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		panelContent.add(panel_1);
+		panel_1.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new TitledBorder(null, "Details", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.add(panel_3);
+		GridBagLayout gbl_panel_3 = new GridBagLayout();
+		gbl_panel_3.columnWidths = new int[]{80, 0, 130, 0};
+		gbl_panel_3.rowHeights = new int[]{50, 50, 0};
+		gbl_panel_3.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_3.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panel_3.setLayout(gbl_panel_3);
+		
+		JLabel lblSupervisorName = new JLabel("Supervisor Name");
+		lblSupervisorName.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblSupervisorName = new GridBagConstraints();
+		gbc_lblSupervisorName.weightx = 0.3;
+		gbc_lblSupervisorName.anchor = GridBagConstraints.WEST;
+		gbc_lblSupervisorName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSupervisorName.gridx = 0;
+		gbc_lblSupervisorName.gridy = 0;
+		panel_3.add(lblSupervisorName, gbc_lblSupervisorName);
+		
+		textSupervisorName = new JTextField();
+		textSupervisorName.setColumns(10);
+		GridBagConstraints gbc_textSupervisorName = new GridBagConstraints();
+		gbc_textSupervisorName.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textSupervisorName.insets = new Insets(0, 0, 5, 5);
+		gbc_textSupervisorName.gridx = 1;
+		gbc_textSupervisorName.gridy = 0;
+		panel_3.add(textSupervisorName, gbc_textSupervisorName);
+		
+		JPanel panel_2 = new JPanel();
+		panel_1.add(panel_2);
+		panel_2.setLayout(new GridLayout(2, 0, 0, 0));
+		
+		JPanel panel_4 = new JPanel();
+		panel_2.add(panel_4);
+		panel_4.setLayout(new GridLayout(0, 10, 0, 0));
+		
+		JButton btnAdd = new JButton("Add");
+		btnAdd.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		panel_4.add(btnAdd);
+		
+		JButton btnChange = new JButton("Change");
+		btnChange.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		panel_4.add(btnChange);
+		
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		panel_4.add(btnDelete);
+		
+		JButton btnViewRoom = new JButton("View Room");
+		btnViewRoom.setVisible(false);
+		btnViewRoom.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		panel_4.add(btnViewRoom);
+		
+		JPanel panel_5 = new JPanel();
+		panel_2.add(panel_5);
+		panel_5.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_6 = new JPanel();
+		panel_5.add(panel_6, BorderLayout.SOUTH);
+		GridBagLayout gbl_panel_6 = new GridBagLayout();
+		gbl_panel_6.columnWidths = new int[]{256, 256, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_6.rowHeights = new int[]{21, 0};
+		gbl_panel_6.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_6.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel_6.setLayout(gbl_panel_6);
+		
+		textSearch = new JTextField();
+		textSearch.setHorizontalAlignment(SwingConstants.LEFT);
+		textSearch.setColumns(10);
+		GridBagConstraints gbc_textSearch = new GridBagConstraints();
+		gbc_textSearch.fill = GridBagConstraints.BOTH;
+		gbc_textSearch.anchor = GridBagConstraints.WEST;
+		gbc_textSearch.gridwidth = 2;
+		gbc_textSearch.insets = new Insets(0, 0, 0, 5);
+		gbc_textSearch.gridx = 0;
+		gbc_textSearch.gridy = 0;
+		panel_6.add(textSearch, gbc_textSearch);
+		
+		JButton btnSearch = new JButton("Search");
+		btnSearch.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		GridBagConstraints gbc_btnSearch = new GridBagConstraints();
+		gbc_btnSearch.fill = GridBagConstraints.BOTH;
+		gbc_btnSearch.anchor = GridBagConstraints.WEST;
+		gbc_btnSearch.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSearch.gridx = 2;
+		gbc_btnSearch.gridy = 0;
+		panel_6.add(btnSearch, gbc_btnSearch);
+		
+		JButton btnLoad = new JButton("Load data");
+		btnLoad.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		GridBagConstraints gbc_btnLoad = new GridBagConstraints();
+		gbc_btnLoad.anchor = GridBagConstraints.EAST;
+		gbc_btnLoad.gridx = 14;
+		gbc_btnLoad.gridy = 0;
+		panel_6.add(btnLoad, gbc_btnLoad);
+		
+		JPanel panel = new JPanel();
+		panelContent.add(panel);
+		panel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel.add(scrollPane);
+		
+		tableSupervisor = new JTable();
+		scrollPane.setViewportView(tableSupervisor);
+
+	}
+
+}
